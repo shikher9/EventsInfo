2bb69f6e25924c3253a9b3a338cb9177950c9fb8
Shikher Pandey <shikher9@gmail.com>
2016-12-31 02:42:38 -0800

    changed port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var events = require("./eventsController");
 var bodyParser = require("body-parser");
 var fs = require("fs");
 
-
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -23,6 +23,10 @@ app.get("/data/events", function (req, res) {
     res.send(events);
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+app.get("/", function (req, res) {
+    res.sendfile("index.html");
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
